5386daa660e5c5374f52dce022651c159ecdf4bd
DiegoFernandez-DF <diegoestefer@gmail.com>
2026-05-08 13:49:05 -0300

    feat: completa views.py

--- publicaciones/views.py
+++ publicaciones/views.py
@@ -22,6 +22,14 @@ from .models import Publicacion
 #           context["mensaje"] = "..."
 #           return context
 
+class InicioView(TemplateView):
+    template_name = "publicaciones/inicio.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["titulo"] = "Portal de publicaciones"
+        context["mensaje"] = "Bienvenido/a al sitio"
+        return context
 
 # ---------------------------------------------------------------------------
 # PublicacionListView
@@ -38,6 +46,10 @@ from .models import Publicacion
 #       model = ...
 #       context_object_name = "..."
 
+class PublicacionListView(ListView):
+    model = Publicacion
+    context_object_name = "publicacion_list"
+    template_name = "publicaciones/publicacion_list.html"
 
 # ---------------------------------------------------------------------------
 # PublicacionDetailView
@@ -57,3 +69,9 @@ from .models import Publicacion
 #       model = ...
 #       context_object_name = "..."
 #       pk_url_kwarg = "..."
+
+class PublicacionDetailView(DetailView):
+    model = Publicacion
+    context_object_name = "publicacion"
+    pk_url_kwarg = "publicacion_id"
+    template_name = "publicaciones/publicacion_detail.html"
